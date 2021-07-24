54e2454bcfe8f1f2490e4fc2fbe6e2562a161ff5
desirel <two_somang@icloud.com>
2021-07-25 00:35:07 +0900

    feat: Spring Batch Project 초기화

---
+++ src/main/java/twosom/spring/batch/SpringBatchExampleApplication.java
@@ -0,0 +1,13 @@
+package twosom.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/twosom/spring/batch/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package twosom.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
