abedb8d8a41e0b4e3f8233d4f9dff7c3745b9a79
desirel <two_somang@icloud.com>
2021-07-27 23:14:02 +0900

    feat: JDBC 데이터 쓰기

--- src/main/java/twosom/spring/batch/part3/Person.java
+++ src/main/java/twosom/spring/batch/part3/Person.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 @Entity
 public class Person {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String age;
